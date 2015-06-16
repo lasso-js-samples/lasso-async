4696f2dcad8c2d12d039105bf2a5a056ea40fd5b
Patrick Steele-Idem <pnidem@gmail.com>
2015-06-16 13:44:11 -0600

    Initial commit

---
+++ main.js
@@ -0,0 +1,23 @@
+var buttonEl = document.getElementById('runButton');
+buttonEl.addEventListener('click', function() {
+    // Asynchronously load the "./add" and "jquery" modules, as well
+    // as the "style.less" file:
+    require('raptor-loader').async(
+        ['style.less'],
+        function() {
+            // Everything that is required within this function will be
+            // asynchronously downloaded. In additional dependencies passed
+            // in using the first argument to the async method will also
+            // be asynchronously downloaded. Whenever thing has been loaded
+            // by the web broweer the following code will run. If all of
+            // the dependencies have already been downloaded then the function
+            // will be invoked immediately.
+            var add = require('./add');
+            var jquery = require('jquery');
+
+            var now = new Date().toString();
+            var html = '2+2=' + add(2, 2) + ' (' + now + ')';
+
+            jquery('#target').html(html);
+        });
+});
---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+lasso ./my-page.browser.json --inject-into my-page.html --config lasso-config.json
+echo "Open ./my-page.html to try in your web browser!"
